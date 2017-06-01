16a17eafc53fcf53e0d4bc1dd77bf90d7888d3ee
Andy Johnson <andyjohnson70@gmail.com>
2017-05-31 20:43:11 -0700

    Put the top movies by year and genre in the main file

--- server.R
+++ server.R
@@ -21,6 +21,18 @@ topRatedMovies <- function(rating) {
   return (df)
 }
 
+# Function to retrieve information from the API
+topMoviesByYearGenre <- function(year, genre) {
+  key <- "7a6c78ba5a3085b57bf936f116cd1259"
+  base <- paste0("https://api.themoviedb.org/3/discover/movie?with_genres=", genre, "&primary_release_year=", year ,"&sort_by=popularity.desc")
+  query.params <- list(api_key = key)
+  response <- GET(base, query = query.params)
+  body <- content(response, "text")
+  results <- fromJSON(body)
+  info <- results$results
+  df <- data.frame(info$title,info$release_date, info$overview, info$popularity, stringsAsFactors = FALSE)
+}
+
 function(input, output) {
   # Generate wordcloud with buildWordCloud function
   output$plot <- renderPlot({
@@ -66,4 +78,18 @@ function(input, output) {
       layout(title = 'Top 20 Current Most Popular Movies By Rating', xaxis= ax, yaxis=y)
     
   })
+  
+  output$table <- renderTable({
+    
+    data <- topMoviesByYearGenre(input$year, input$genre)
+    
+    colnames(data)[1] <- "Title"
+    colnames(data)[2] <- "Release Date"
+    colnames(data)[3] <- "Overview"
+    colnames(data)[4] <- "Popularity Rating"
+    
+    data
+    
+  })
+  
 }
