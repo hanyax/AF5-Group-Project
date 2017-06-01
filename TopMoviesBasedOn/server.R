4baa18791ac894386443a4e240ac92dcc56ef346
Andy Johnson <andyjohnson70@gmail.com>
2017-05-31 17:27:11 -0700

    Started my part of the project

---
+++ TopMoviesBasedOn/server.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(jsonlite)
+
+topMoviesByYearGenre <- function(year, genre) {
+  key <- "7a6c78ba5a3085b57bf936f116cd1259"
+  base <- paste0("https://api.themoviedb.org/3/discover/movie?with_genres=", genre, "&primary_release_year=", year ,"&sort_by=vote_average.desc")
+  query.params <- list(api_key = key)
+  response <- GET(base, query = query.params)
+  body <- content(response, "text")
+  results <- fromJSON(body)
+  info <- results$results
+  df <- data.frame(info$title,info$release_date, info$popularity, stringsAsFactors = FALSE)
+}
+
+shinyServer(function(input, output) {
+  
+  
+})
